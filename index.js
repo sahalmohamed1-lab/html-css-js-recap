07600231c99a4d7c14ec88dfbbf55c6fddc4a547
sahalmohamed1 <sahal.mohamed1@student.moringaschool.com>
2026-03-16 15:46:56 +0300

    Added js code

---
+++ index.js
@@ -0,0 +1,10 @@
+const loc = "Ngong Road, Nairobi, Kenya";
+// loc = "Kiambu Road, Nairobi, Kenya";
+let age = 25;
+age = 26;
+var StudentName = "Sahal";
+StudentName = "Sahal Ahmed";
+
+//console.log(loc);
+//console.log(age);
+console.log(StudentName);
